2e29623ada49c8ef39a44edec5f290d0a54e1d3e
Jaydeep Singh Chouhan <jaydeepchouhan20808@acropolis.in>
2023-09-01 19:19:26 +0530

    first commit

---
+++ src/com/pharmacystore/connection/DbDetails.java
@@ -0,0 +1,9 @@
+package com.pharmacystore.connection;
+
+public interface DbDetails {
+
+	String CONSTR = "jdbc:mysql://localhost:3306/pharmacystoredb";
+	String USERNAME = "root";
+	String PASSWORD = "Aeishh!!";
+	String DBDRIVER ="com.mysql.cj.jdbc.Driver";		//  "com.mysql.jdbc.Driver"
+}
